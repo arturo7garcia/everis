7d9255051235e1e7e8660a63448ef7bf0dde7e46
Arturo Garcia <arturocarcabuey@gmail.com>
2021-11-22 12:38:37 +0100

    first commit

---
+++ src/main/java/com/everis/mvnagc/App.java
@@ -0,0 +1,15 @@
+package com.everis.mvnagc;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
